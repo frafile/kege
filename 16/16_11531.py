51a47eeb5c59c30bd19aff9416de8d47d696cc0a
aleksei.semikashev <aleksei.semikashev@mobirate.com>
2026-05-25 19:46:20 +0400

    16

---
+++ 16/16_11531.py
@@ -0,0 +1,11 @@
+from functools import *
+
+@cache
+def F(n):
+    if n < 10:
+        return n
+    # return F(n % 10) + F(n // 10)
+    return n % 10 + F(n // 10)
+
+for i in range(0, 2**63):
+    F(i)
